c068ff5d5c3c7e2f053c846acd86d27c15a1649c
zxlwq <zxlwq919@gmail.com>
2025-03-21 23:01:50 +0800

    Site updated: 2025-03-21 23:01:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/21/这是一篇新的博文/"];function toRandomPost(){
+var posts=["2025/03/21/搭建个人博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
